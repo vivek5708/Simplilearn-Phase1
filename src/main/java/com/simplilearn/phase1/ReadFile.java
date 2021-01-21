12d75b73b68ee09001b5fccb2b8163064e240298
unknown <vivekkumar5708@gmail.com>
2021-01-21 17:30:49 +0530

    added sorting technique for displaying files

--- src/main/java/com/simplilearn/phase1/ReadFile.java
+++ src/main/java/com/simplilearn/phase1/ReadFile.java
@@ -1,21 +1,29 @@
 package com.simplilearn.phase1;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ReadFile {
 	
 		public void readFiles() {
 			File file = new File("D:\\Simplilearn Assessment");
 			File[] files =  file.listFiles();
+			List<String> sortedFiles =  new ArrayList<String>();
 			System.out.println("No. of Files: "+ files.length);
 			for(File eachfile: files) {
 				if(eachfile.isDirectory()) {
 					File[] folderFiles = eachfile.listFiles();
 					for(File eachFolderFiles: folderFiles) {
-						System.out.println(eachFolderFiles.getName());
+						sortedFiles.add(eachFolderFiles.getName());
 					}
 				}
-				System.out.println(eachfile.getName());
+				sortedFiles.add(eachfile.getName());
+			}
+			Collections.sort(sortedFiles);
+			for(String files1: sortedFiles) {
+				System.out.println(files1);
 			}
 		}
 }
